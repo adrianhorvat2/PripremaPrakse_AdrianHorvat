0066bfe0ad2dc95e9147619477299cca75dc0753
adrianhorvat2 <adro.horvat@gmail.com>
2025-12-04 17:35:33 +0100

    Solve task 1 - Create component counter

---
+++ src/app/components/zad1/counter/counter.ts
@@ -0,0 +1,25 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.html',
+  styleUrl: './counter.scss',
+})
+export class Counter {
+
+  count = signal(0);
+
+  increment(){
+    this.count.update(value => value + 1);
+  }
+
+  decrement() {
+    this.count.update(value => value - 1);
+  }
+
+  reset() {
+    this.count.set(0);
+  }
+  
+}
