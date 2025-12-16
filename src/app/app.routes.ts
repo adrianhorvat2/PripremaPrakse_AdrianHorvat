ca790d90801ce2823840a22b55ba5759a19cc526
adrianhorvat2 <adro.horvat@gmail.com>
2025-12-16 22:46:36 +0100

    Implement routing (part2)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Counter } from './components/zad1/counter/counter';
+import { FilterProducts } from './components/zad2/filter-products/filter-products';
+import { UserManagement } from './components/zad5/user-management/user-management';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'zad1', component: Counter },
+  { path: 'zad2', component: FilterProducts },
+  { path: 'zad5', component: UserManagement },
+];
